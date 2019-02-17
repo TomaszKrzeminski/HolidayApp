511c8f4ffed7d5ec77d519df7b1611affcc790d8
Tomasz Krzeminski <koral2323@gmail.com>
2019-02-17 10:21:41 +0100

    First commit

---
+++ HolidayApp/HolidayApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HolidayApp.Startup))]
+namespace HolidayApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
